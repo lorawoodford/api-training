08fac5ca28af01533e2ade65f182ce7753e479d5
lorajdavis <username@users.noreply.github.com>
2017-04-07 15:14:41 -0400

    postBarcodes pt. 2

---
+++ postBarcodes.py
@@ -0,0 +1,49 @@
+import json, requests, secrets, time, csv, os
+
+startTime = time.time()
+
+# import secrets
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+
+#authenticate
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+
+# test for successful connection
+def test_connection():
+	try:
+		requests.get(baseURL)
+		print 'Connected!'
+		return True
+
+	except requests.exceptions.ConnectionError:
+		print 'Connection error. Please confirm ArchivesSpace is running.  Trying again in 10 seconds.'
+
+is_connected = test_connection()
+
+while not is_connected:
+	time.sleep(10)
+	is_connected = test_connection()
+
+# print instructions
+print 'This script replace existing fauxcodes with real barcodes (linked in a separate csv file) in ArchivesSpace.'
+
+# open csv and generate dict
+reader = csv.DictReader(open('barcodes.csv'))
+
+# GET each top_container listed in top_containers and add to records
+for row in reader:
+	uri = row['uri']
+	output = requests.get(baseURL + uri, headers=headers).json()
+	output['barcode'] = row['real']
+	post = requests.post(baseURL + uri, headers=headers, data=json.dumps(output)).json()
+	print post
+
+# show script runtime
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Post complete.  Total script run time: ', '%d:%02d:%02d' % (h, m, s)
